fa83d4e2540eddcfa2e4f201ea20f1d766c8a747
Tomas <tomas.fermoso@gmail.com>
2020-07-09 16:09:53 +0200

    theme child with new sidebar

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+function my_theme_enqueue_styles() {
+
+ $parent_style = 'parent-style'; // Estos son los estilos del tema padre recogidos por el tema hijo.
+
+ wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+ wp_enqueue_style( 'child-style',
+ get_stylesheet_directory_uri() . '/style.css',
+ array( $parent_style ),
+ wp_get_theme()->get('Version')
+ );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+/* Código para crear un widget */
+if (function_exists('register_sidebar')) {
+    register_sidebar(array(
+        'name' => 'Nuevos Widgets',
+        'id'   => 'sidebar-3',
+        'description'   => 'These are widgets for the sidebar.',
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2>',
+        'after_title'   => '</h2>'
+    ));
+}
+
+?>
