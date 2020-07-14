676662bd23d63f7cef6ed97b779393339a167c2f
Fani-19 <estefbd@hotmail.com>
2020-07-14 20:28:38 +0200

    Add nueva plantilla y copiar contenido

---
+++ template/nueva -pantilla.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Nueva plantilla
+ * Template Post Type: post, page
+ *
+ * @package Wordpress
+ * @subpackage Twenty_Twenty_Child
+ * @since Twenty Twenty Child 1.0
+ */
+
+get_header();
+?>
+<div id="main">
+
+
+<?php
+
+if (have_posts()) {
+
+    while (have_posts()) {
+        the_post();
+        ?>
+<article <?php post_class();?> id="post-<?php the_ID();?>">
+<div class="cabecera">
+
+<img src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'twentytwenty-fullscreen' );?>">
+<?php the_title("<h1>", "</h1>", true);?>
+    </div>
+    <div class="contenido">
+    <nav class="navegacion">
+    algo
+    </nav>
+    <section>
+        <?php echo the_content();?>
+    </section>
+    </div>
+    </article>
+       <?php
+}
+}
+?>
+</div>
