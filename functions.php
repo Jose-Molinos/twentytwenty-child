de19a86ddaaad8afd22704d32fcf871a8c1405ed
Jose Molinos <molinossobrino.jose@gmail.com>
2020-07-15 19:53:08 +0200

    15_07_2020

--- functions.php
+++ functions.php
@@ -26,4 +26,46 @@ if (function_exists('register_sidebar')) {
     ));
 }
 
+
+// Incluir Bootstrap CSS
+function bootstrap_css() {
+	wp_enqueue_style( 'bootstrap_css', 
+  					'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css', 
+  					array(), 
+  					'4.1.3'
+  					); 
+}
+add_action( 'wp_enqueue_scripts', 'bootstrap_css');
+
+
+// Incluir Bootstrap JS y dependencia popper
+function bootstrap_js() {
+	wp_enqueue_script( 'popper_js', 
+  					'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', 
+  					array(), 
+  					'1.14.3', 
+  					true); 
+	wp_enqueue_script( 'bootstrap_js', 
+  					'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', 
+  					array('jquery','popper_js'), 
+  					'4.1.3', 
+  					true); 
+}
+add_action( 'wp_enqueue_scripts', 'bootstrap_js');
+
+/**
+ * Register navigation menus uses wp_nav_menu in five places.
+ */
+function twentytwenty_child_menus() {
+
+	$locations = array(
+		'vertical'  => __( 'Menu Vertical Plantilla', 'twentytwentychild' ),
+		
+	);
+
+	register_nav_menus( $locations );
+}
+
+add_action( 'init', 'twentytwenty_child_menus' );
+
 ?>
---
+++ templates/nueva-plantilla.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Template Name: Nueva plantilla
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+get_header();
+?>
+
+<div id="main">
+<?php
+
+if (have_posts()) {
+
+    while (have_posts()) {
+        the_post();
+        ?>
+        <article <?php post_class();?> id="post-<?php the_ID();?>">
+    <div class="cabecera row">
+        <img class="col-sm-4"src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'twentytwenty-fullscreen'); ?>"></img>
+        <?php the_title("<h1 class='col-sm-8'>", "</h1>", true);?>
+        </div>
+        <div class="row">
+            <nav class="col-sm-4">
+
+            <?php
+if (has_nav_menu('primary')) {
+            ?>
+
+							<nav class="primary-menu-wrapper" aria-label="<?php esc_attr_e('Horizontal', 'twentytwenty');?>" role="navigation">
+
+								<ul class="menu_vertical">
+
+								<?php
+if (has_nav_menu('vertical')) {
+
+                wp_nav_menu(
+                    array(
+                        'container' => '',
+                        'items_wrap' => '%3$s',
+                        'theme_location' => 'vertical',
+                    )
+                );
+
+            }
+            ?>
+
+								</ul>
+
+							</nav><!-- .primary-menu-wrapper -->
+
+						<?php
+}
+        ?>
+            </nav>
+            <section class='col-sm-8 contenido_principal' >
+                <?php echo the_content(); ?>
+            </section>
+        </div>
+
+
+        </article>
+
+        <?php
+}
+}
+
+?>
+
+</div>
+
+<?php get_template_part('template-parts/footer-menus-widgets');?>
+
+<?php get_footer(); ?>
